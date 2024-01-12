b5ee57d1998ff333b7607648956b660ba13d6241
FarzadVav <farzad.vav.work@gmail.com>
2024-01-11 23:43:32 -0800

    add codes: RenderLoading.tsx

--- src/components/RenderLoading.tsx
+++ src/components/RenderLoading.tsx
@@ -1,5 +1,46 @@
-const RenderLoading = () => {
-  return <div>RenderLoading</div>
+import { ReactNode, useContext, useEffect, useState } from "react"
+
+import Loading from "../contexts/Loading"
+
+type RenderLoadingProps = {
+  keys?: string[]
+  page?: boolean
+  loadingComponent?: ReactNode
+  children?: ReactNode
+}
+
+const RenderLoading = (props: RenderLoadingProps) => {
+  const loading = useContext(Loading)
+  const [loaders, setLoaders] = useState<{ page: boolean; action: boolean }>({
+    page: false,
+    action: false,
+  })
+
+  useEffect(() => {
+    if (props.page) {
+      setLoaders((prev) => ({
+        ...prev,
+        page: loading.keys.filter((key) => key.type === "PAGE").length > 0,
+      }))
+    }
+
+    if (props.keys?.length) {
+      let hasKey = false
+      loading.keys.forEach((key) => {
+        props.keys?.forEach((_key) => {
+          if (key.type === "ACTION" && key.name === _key) hasKey = true
+        })
+      })
+      setLoaders((prev) => ({ ...prev, action: hasKey }))
+    }
+  }, [loading])
+
+  return (
+    <>
+      <div className={`loading ${loaders.page ? "loading-on" : "loading-off"}`}></div>
+      {loaders.action ? props.loadingComponent : props.children}
+    </>
+  )
 }
 
 export default RenderLoading
