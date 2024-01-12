ebb88e79356e6d21b7628010ce92250adbdf9455
FarzadVav <farzad.vav.work@gmail.com>
2024-01-12 00:01:36 -0800

    fixed show loading

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,25 @@
+import { useContext, useEffect } from "react"
 import { useRoutes } from "react-router-dom"
 
 import routes from "./routes"
+import LoadingContext from "./contexts/Loading"
 import Header from "./components/Header"
 import RenderLoading from "./components/RenderLoading"
 
 const App = () => {
   const router = useRoutes(routes)
+  const loading = useContext(LoadingContext)
+
+  useEffect(() => {
+    fetch("https://fakestoreapi.com/products")
+      .then(() => {
+        // codes...
+      })
+      .catch(() => {
+        // codes...
+      })
+      .finally(() => loading.removeKey())
+  }, [])
 
   return (
     <>
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,11 +5,7 @@ const Header = () => {
   return (
     <header className="bg-slate-900 w-full flex justify-center items-center gap-6 px-6 py-3">
       {routes.map((route) => (
-        <Link
-          className="link"
-          activeClassName="active-link"
-          path={route.path}
-        >
+        <Link key={route.path} className="link" activeClassName="active-link" path={route.path}>
           {route.name}
         </Link>
       ))}
