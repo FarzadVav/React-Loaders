f84ab140a91b21fe7fef4c2a5e76db8be23542a4
FarzadVav <farzad.vav.work@gmail.com>
2024-01-12 00:21:18 -0800

    fixed loading for buttons

--- src/pages/Actions.tsx
+++ src/pages/Actions.tsx
@@ -1,4 +1,5 @@
 import useLoadingEffect from "../hooks/useLoadingEffect"
+import Button from "../components/Button"
 
 const Actions = () => {
   useLoadingEffect((cl) => {
@@ -12,9 +13,33 @@ const Actions = () => {
       .finally(() => cl())
   })
 
+  const submitHandler = (cl: Function) => {
+    fetch("https://fakestoreapi.com/products")
+      .then(() => {
+        // codes...
+      })
+      .catch(() => {
+        // codes...
+      })
+      .finally(() => cl())
+  }
+
   return (
     <div className="wrapper">
       <h1>Actions</h1>
+      {/* Buttons */}
+      <div className="flex items-center gap-3 mt-6">
+        <Button className="button" clickHandler={(cl) => submitHandler(cl)}>
+          SUBMIT 1
+        </Button>
+        <Button className="button" clickHandler={(cl) => submitHandler(cl)}>
+          SUBMIT 2
+        </Button>
+        <Button className="button" clickHandler={(cl) => submitHandler(cl)}>
+          SUBMIT 3
+        </Button>
+      </div>
+      {/* Buttons */}
       <p>
         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Labore magni ipsam ut, enim dolor
         harum quo molestiae recusandae veritatis consequuntur et vero sed aperiam quidem amet? Animi
